d21512769e34429426f2d5f56edc43d7b743e82b
IGOR VINICIUS SANTOS VICENTE <47865879822@info.local>
2026-03-09 18:56:52 -0300

    Cáculo certo da mediana

--- projeto-estatistica.py
+++ projeto-estatistica.py
@@ -1,5 +1,5 @@
 # Projeto Colaborativo - Ciencia de Dados
-dados_sujos = [10 , " erro ", 20 , 30 , 40 , None , 50 , 15 , " falha ", 25]
+dados_sujos = [10, " erro ", 20, 30, 40, None, 50, 15, " falha ", 25]
 
 def limpar_dados():
     # Retorne uma lista apenas com int ou float
@@ -8,8 +8,21 @@ def limpar_dados():
 def calcular_media():
     pass
 
-def calcular_mediana():
-    pass
+# igor
+def calcular_mediana(dados):
+    dados_ordenados = sorted(dados)
+    n = len(dados_ordenados)
+
+    if n % 2 == 0:
+        meio1 = dados_ordenados[n//2 - 1]
+        meio2 = dados_ordenados[n//2]
+        return (meio1 + meio2) / 2
+    else:
+        return dados_ordenados[n//2]
+
+mediana = calcular_mediana(dados)
+print(f"Dados processados: {dados}")
+print(f"Mediana dos dados: {mediana}")
 
 def calcular_variancia():
     pass
@@ -17,6 +30,4 @@ def calcular_variancia():
 def obter_extremos():
     pass
 
-dados = limpar_dados(dados_sujos)
-
-print(f" Dados processados : {dados}")
+dados = limpar_dados(dados_sujos)   
